015c246c7f7f31b22d43c14c32a85d7aa8225273
Clement lemarie <big_m@live.fr>
2025-08-22 17:09:28 +0200

    installation vue et tailwind

---
+++ assets/vue/controllers/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 class="text-3xl font-bold underline"> Home controller in Vue</h2>
+    <div>Hello {{ user_name }}!</div>
+</template>
+
+<script setup>
+    defineProps({
+        user_name: String
+    });
+</script>
