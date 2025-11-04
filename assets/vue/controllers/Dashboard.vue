78160655a50fb6c052759730e1d26d36bb57e3da
Clement lemarie <big_m@live.fr>
2025-11-04 16:53:54 +0100

    dashboard

--- assets/vue/controllers/Dashboard.vue
+++ assets/vue/controllers/Dashboard.vue
@@ -8,9 +8,11 @@
 
 //TODO:
 // Ajouter une modal de validation pour les opérations importantes (undocking et reset)
+// Travailler sur la modal de validation
 // gestion du animate pulse si chargement de la list
 // ajout d'un système de notification
 // ça serait cool de passer à la reception avant induction
+//
 
 import { onMounted } from 'vue'
 import BorderedContent from './UI/BorderedContent.vue';
