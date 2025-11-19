d2a2daa798b3926bace2cb5fd384a956973b6e38
Clement lemarie <big_m@live.fr>
2025-11-19 18:46:01 +0100

    fix overlay

--- assets/vue/controllers/UI/OverlayInvisible.vue
+++ assets/vue/controllers/UI/OverlayInvisible.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-0 right-0 h-screen w-screen" @click="emit('click')"></div>
+  <div class="fixed top-0 right-0 h-screen w-screen z-10" @click="emit('click')"></div>
 </template>
 
 <script setup>
