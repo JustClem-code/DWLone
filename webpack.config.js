b346437f0197ca83bd4a9cd9cdde6a7e6c33213b
Clement lemarie <big_m@live.fr>
2025-12-18 18:03:19 +0100

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    // .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.ts')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -67,7 +68,10 @@ Encore
     //.enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    // .enableTypeScriptLoader()
+    .enableTypeScriptLoader((tsConfig) => {
+        // options ts-loader éventuelles
+    })
 
     // uncomment if you use React
     //.enableReactPreset()
