55b3202e0ea128bbe5c23595c762515253be3214
Clement lemarie <big_m@live.fr>
2026-01-13 17:58:14 +0100

    WiP on get bags with packages

--- assets/vue/controllers/Dashboard.vue
+++ assets/vue/controllers/Dashboard.vue
@@ -12,7 +12,13 @@
 
 //TODO:
 
+// problème de circular référence pour les bags
+// créer un vignette Bag sur le dashboard
+
+
 // finir la logic de stow côter vue et php -> vérifier
+
+// revoir la gestion de la mise en Bag, regrouper les Bags des routes dans la même allée si possible
 
 // gestion du animate pulse si chargement de la (list / HorizontalLinkButton et divers composants) => voir tailwind UI
 // plus globalement, il faut gérer le chargement côté vue et twig
