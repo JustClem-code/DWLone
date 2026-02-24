d4096c5ba429fbe9a99530921b4b79baf7bff115
Clement lemarie <big_m@live.fr>
2026-02-24 17:47:21 +0100

    fix with in mobile for infocomponent in dialog component

--- assets/vue/controllers/UI/Modals/DialogComponentSlot.vue
+++ assets/vue/controllers/UI/Modals/DialogComponentSlot.vue
@@ -2,7 +2,7 @@
   <dialog ref="myDialog" @click.self="closeDialog">
     <OverlayInvisible @click="closeDialog" />
     <div
-      class="text-gray-900 dark:text-gray-100 border border-0 dark:border-1 dark:border-gray-700/90 sm:w-full md:w-max max-w-[95vw] max-h-[95vh] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-8 rounded-2xl shadow-2xl"
+      class="text-gray-900 dark:text-gray-100 border border-0 dark:border-1 dark:border-gray-700/90 w-full md:w-max max-w-[95vw] max-h-[95vh] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 p-8 rounded-2xl shadow-2xl overflow-y-auto scrollbar-dark"
       :class="darkerBackground ? 'bg-white dark:bg-gray-900' : 'bg-white dark:bg-gray-800'">
       <slot></slot>
       <div v-show="hasCloseCross" class="absolute top-2 right-2 block p-3">
