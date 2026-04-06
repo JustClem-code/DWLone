b449e195dcafe0f5e35b34e924e07bd5f5680f29
Clement lemarie <big_m@live.fr>
2026-04-06 17:20:51 +0200

    WIP picking

--- assets/vue/controllers/PickingComponents/FloorStaggingArea.vue
+++ assets/vue/controllers/PickingComponents/FloorStaggingArea.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid grid-cols-4 divide-x divide-gray-200 dark:divide-gray-700/90">
+  <div class="grid grid-flow-col grid-rows-2 gap-4">
 
-    <div v-for="(groupe, indexGroup) in staggingAreas" :key="indexGroup" class="flex flex-col gap-1 sm:gap-4 justify-center px-2 md:px-4">
+    <div v-for="(groupe, indexGroup) in staggingAreas" :key="indexGroup" class="grid grid-cols-4 gap-1 sm:gap-4 justify-center px-2 md:px-4">
 
       <HorizontalLinkButton v-for="(staggingArea) in groupe" :key="staggingArea" @click="action(staggingArea)" :title="staggingArea.name"
         :focused="alleyHasPackages(staggingArea) ? 'text-blue-400' : 'text-gray-300 dark:text-gray-700/90'"
