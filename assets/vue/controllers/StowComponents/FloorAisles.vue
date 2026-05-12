6338db2e5e5b55450f6fdaf3e9db79921870b707
Clement lemarie <big_m@live.fr>
2026-05-12 17:42:00 +0200

    refacto function in stow component

--- assets/vue/controllers/StowComponents/FloorAisles.vue
+++ assets/vue/controllers/StowComponents/FloorAisles.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="grid grid-cols-4 divide-x divide-gray-200 dark:divide-gray-700/90">
 
-    <div v-for="(groupe, indexGroup) in locations" :key="indexGroup" class="flex flex-col gap-1 sm:gap-4 justify-center px-2 md:px-4">
+    <div v-for="(groupe, indexGroup) in locations" :key="indexGroup"
+      class="flex flex-col gap-1 sm:gap-4 justify-center px-2 md:px-4">
 
       <HorizontalLinkButton v-for="(pair) in groupe" :key="pair" @click="setCurrentPair(pair)" :title="pair.id"
         :focused="alleyHasPackages(pair) ? 'text-blue-400' : 'text-gray-300 dark:text-gray-700/90'"
@@ -23,8 +24,13 @@ const props = defineProps({
 
 const { setCurrentPair } = inject('stow')
 
-const alleyHasPackages = (pair) => {
+/* const alleyHasPackages = (pair) => {
   const ar = pair.locations.flatMap(row => row.packages.filter(p => p.userStow === null))
   return ar.length !== 0
+} */
+
+const alleyHasPackages = (pair) => {
+  return pair.locations.some(row => row.packages.some(p => p.userStow === null))
 }
+
 </script>
